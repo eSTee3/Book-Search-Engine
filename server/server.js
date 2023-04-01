cfacd8387e89f440d1134a66f1c6451e050660e2
eSTee3 <tagsxa@yahoo.com>
2023-04-01 16:07:28 -0500

    updated port

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
   typeDefs,
